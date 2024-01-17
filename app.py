3cf633b3cfc2dbf32bb1626e08a7c4134b1a577c
pandacase <2227119810@qq.com>
2024-01-17 12:52:50 +0800

    update app

--- app.py
+++ app.py
@@ -3,57 +3,38 @@ import pickle
 import requests
 
 def fetch_poster(movie_id):
-     url = "https://api.themoviedb.org/3/movie/{}?api_key=c7ec19ffdd3279641fb606d19ceb9bb1&language=en-US".format(movie_id)
-     data=requests.get(url)
-     data=data.json()
-     poster_path = data['poster_path']
-     full_path = "https://image.tmdb.org/t/p/w500/"+poster_path
-     return full_path
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=c7ec19ffdd3279641fb606d19ceb9bb1&language=en-US".format(movie_id)
+    data = requests.get(url)
+    data = data.json()
+    poster_path = data['poster_path']
+    full_path = "https://image.tmdb.org/t/p/w500/"+poster_path
+    return full_path
 
-movies = pickle.load(open("movies_list.pkl", 'rb'))
-similarity = pickle.load(open("similarity.pkl", 'rb'))
-movies_list=movies['title'].values
-
-st.header("Movie Recommender System")
-
-import streamlit.components.v1 as components
-
-imageCarouselComponent = components.declare_component("image-carousel-component", path="frontend/public")
-
-
-imageUrls = [
-    fetch_poster(1632),
-    fetch_poster(299536),
-    fetch_poster(17455),
-    fetch_poster(2830),
-    fetch_poster(429422),
-    fetch_poster(9722),
-    fetch_poster(13972),
-    fetch_poster(240),
-    fetch_poster(155),
-    fetch_poster(598),
-    fetch_poster(914),
-    fetch_poster(255709),
-    fetch_poster(572154)
-   
-    ]
-
-
-imageCarouselComponent(imageUrls=imageUrls, height=200)
-selectvalue=st.selectbox("Select movie from dropdown", movies_list)
 
 def recommend(movie):
-    index=movies[movies['title']==movie].index[0]
+    index = movies[movies['title']==movie].index[0]
     distance = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda vector:vector[1])
-    recommend_movie=[]
-    recommend_poster=[]
+    recommend_movie = []
+    recommend_poster = []
     for i in distance[1:6]:
-        movies_id=movies.iloc[i[0]].id
+        movies_id = movies.iloc[i[0]].id
         recommend_movie.append(movies.iloc[i[0]].title)
         recommend_poster.append(fetch_poster(movies_id))
     return recommend_movie, recommend_poster
 
 
+# load data
+
+movies = pickle.load(open("./pkls/movies_list.pkl", 'rb'))
+similarity = pickle.load(open("./pkls/similarity.pkl", 'rb'))
+movies_list=movies['title'].values
+
+
+# build page
+
+st.header("Movie Recommender System")
+
+selectvalue = st.selectbox("Select movie from dropdown", movies_list)
 
 if st.button("Show Recommend"):
     movie_name, movie_poster = recommend(selectvalue)
@@ -73,3 +54,27 @@ if st.button("Show Recommend"):
     with col5:
         st.text(movie_name[4])
         st.image(movie_poster[4])
+
+
+
+## For testing
+        
+# import streamlit.components.v1 as components
+# imageCarouselComponent = components.declare_component("image-carousel-component", path="frontend/public")
+# imageUrls = [
+#     fetch_poster(1632),
+#     fetch_poster(299536),
+#     fetch_poster(17455),
+#     fetch_poster(2830),
+#     fetch_poster(429422),
+#     fetch_poster(9722),
+#     fetch_poster(13972),
+#     fetch_poster(240),
+#     fetch_poster(155),
+#     fetch_poster(598),
+#     fetch_poster(914),
+#     fetch_poster(255709),
+#     fetch_poster(572154)
+#     ]
+
+# imageCarouselComponent(imageUrls=imageUrls, height=200)
--- main.py
+++ main.py
@@ -5,21 +5,20 @@ movies=movies[['id', 'title', 'overview', 'genre']]
 movies['tags'] = movies['overview']+movies['genre']
 new_data  = movies.drop(columns=['overview', 'genre'])
 
+#####################################################################
+
 from sklearn.feature_extraction.text import CountVectorizer
 
 cv=CountVectorizer(max_features=10000, stop_words='english')
 vector=cv.fit_transform(new_data['tags'].values.astype('U')).toarray()
 
+#####################################################################
+
 from sklearn.metrics.pairwise import cosine_similarity
 
 similarity=cosine_similarity(vector)
 
-new_data[new_data['title']=="The Godfather"].index[0]
-
-distance = sorted(list(enumerate(similarity[2])), reverse=True, key=lambda vector:vector[1])
-for i in distance[0:5]:
-    print(new_data.iloc[i[0]].title)
-
+#####################################################################
 
 def recommand(movies):
     index=new_data[new_data['title']==movies].index[0]
@@ -29,9 +28,11 @@ def recommand(movies):
 
 recommand("Iron Man")
 
+#####################################################################
+
 import pickle
 
-pickle.dump(new_data, open('movies_list.pkl', 'wb'))
-pickle.dump(similarity, open('similarity.pkl', 'wb'))
+pickle.dump(new_data, open('./pkls/movies_list.pkl', 'wb'))
+pickle.dump(similarity, open('./pkls/similarity.pkl', 'wb'))
 
 pickle.load(open('movies_list.pkl', 'rb'))
